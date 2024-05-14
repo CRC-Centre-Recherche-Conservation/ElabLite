33eee142353fecedbfb693bb4b6427977d75d73e
rayondemiel <maximehumeau44@gmail.com>
2024-05-14 14:45:48 +0200

    metadata forms class

---
+++ models/forms.py
@@ -0,0 +1,91 @@
+from dataclasses import dataclass, field
+from datetime import date
+from dateutil.parser import parse, ParserError
+import streamlit as st
+from typing import List
+
+class BaseForms:
+    pass
+
+
+@dataclass
+class MetadataForms:
+    name: str
+    field_type: str
+    value: str = ''
+    description: str = ''
+    options: List[str] = field(default_factory=list)
+    required: bool = False
+    position: int = -1
+    group_id: int = 0
+    allow_multi_values: bool = False
+
+    def render(self):
+        """
+        Rendering streamlit widgets according to type detected
+        :return: function widget
+        """
+        field_label = self.name.replace('_', ' ').title()
+
+        getattr(self, f"_render_{self.field_type}_field")(field_label)
+
+    @classmethod
+    def generate_form(cls, metadata):
+        """
+        Iteration generation forms
+        :param metadata: dict, field parameters
+        :return: rendering streamlit widgets forms
+        """
+        extra_fields = metadata.get('extra_fields', {})
+        sorted_fields = sorted(extra_fields.items(), key=lambda x: x[1]['position'] if 'position' in x[1] else -1)
+        group_id = cls.group_id
+        for field_name, field_data in sorted_fields:
+            field_type = field_data.get('type', 'text')
+            field_group_id = field_data.get('group_id', 0)
+            if group_id != field_group_id:
+                group_id = field_group_id
+                st.divider()
+            # cleaning parameters
+            field_data.pop('type', None)
+            # execute
+            field_ = cls(field_name, field_type, **field_data)
+            field_.render()
+
+    def _render_text_field(self, label: str):
+        st.text_input(label, value=self.value, key=self.name, help=self.description)
+
+    def _render_select_field(self, label):
+        st.selectbox(label, self.options, index=self.options.index(self.value) if self.value in self.options else 0,
+                     key=self.name, help=self.description)
+
+    def _render_date_field(self, label: str):
+        try:
+            date_exp = parse(self.value)
+            st.date_input(label, value=date_exp, key=self.name, help=self.description)
+        except ParserError:
+            st.date_input(label, value=date.today(), key=self.name, help=self.description)
+
+    def _render_datetime_local_field(self, label: str):
+        st.date_input(label, value=date.today(), key=self.name, help=self.description)
+
+    def _render_checkbox_field(self, label: str):
+        st.checkbox(label, value=self.value, key=self.name, help=self.description)
+
+    def _render_email_field(self, label: str):
+        st.text_input(label, value=self.value, key=self.name, help=self.description)
+
+    def _render_time_field(self, label: str):
+        st.time_input(label, value=self.value, key=self.name, help=self.description)
+
+    def _render_number_field(self, label: str):
+        st.number_input(label, value=self.value, key=self.name, help=self.description)
+
+    def _render_url_field(self, label: str):
+        st.text_input(label, value=self.value, key=self.name, help=self.description)
+
+    def _render_radio_field(self, label: str):
+        st.radio(label, value=self.value, key=self.name, help=self.description)
+
+# One template with st.text_area
+# and all parameters
+# add button step to step metadata 'presentation, metadata, download' (erdirect page) with progress bar ?
--- pages/2-metadata_forms.py
+++ pages/2-metadata_forms.py
@@ -1,55 +1,12 @@
-from datetime import date
+
 import os
 import streamlit as st
 import time
 
-from menu import menu
+from utils.menu import menu
+from models.forms import MetadataForms
 from utils.parser import TemplatesReader
 
-
-def generate_form(metadata):
-    #https://docs.streamlit.io/develop/api-reference/widgets -> do all fields for elab (https://doc.elabftw.net/metadata.html)
-    # check required option
-    # how to add unit for number
-    # add regex check for input (url, email)
-    extra_fields = metadata.get('extra_fields', {})
-    for field_name, field_data in extra_fields.items():
-        field_type = field_data.get('type')
-        field_label = field_name.replace('_', ' ').title()
-        field_value = field_data.get('value', '')
-        field_required = field_data.get('required', False)
-        field_description = field_data.get('description', '')
-
-        if field_type == 'text':
-            st.text_input(field_label, value=field_value, key=field_name, help=field_description)
-        elif field_type == 'select':
-            options = field_data.get('options', [])
-            st.selectbox(field_label, options, index=options.index(field_value) if field_value in options else 0,
-                         key=field_name, help=field_description)
-        elif field_type == 'date':
-            st.date_input(field_label, value=False, key=field_name, help=field_description)
-        elif field_type == 'datetime-local':
-            st.date_input(field_label, value=date.today(), key=field_name, help=field_description)
-        elif field_type == 'checkbox':
-            st.checkbox(field_label, value=field_value, key=field_name, help=field_description)
-        elif field_type == 'email':
-            st.text_input(field_label, value=field_value, key=field_name, help=field_description)
-        elif field_type == 'time':
-            st.time_input(field_label, value=field_value, key=field_name, help=field_description)
-        elif field_type == 'number':
-            # https://blog.streamlit.io/introducing-new-layout-options-for-streamlit/
-            st.number_input(field_label, value=field_value, key=field_name, help=field_description)
-        elif field_type == 'url':
-            st.text_input(field_label, value=field_value, key=field_name, help=field_description)
-        elif field_type == 'radio':
-            st.radio(field_label, value=field_value, key=field_name, help=field_description)
-        else:
-            raise st.error(f"Error form generation for {field_name, field_data}")
-
-
-# One template with st.text_area
-# and all parameters
-# add button step to step metadata 'presentation, metadata, download' (erdirect page) with progress bar ?
 
 st.title("View Template")
 menu()
@@ -60,7 +17,7 @@ if "selected_template" in st.session_state and os.path.exists(st.session_state["
 
     try:
         template_metadata = reader.read_metadata()
-        generate_form(template_metadata)
+        MetadataForms.generate_form(template_metadata)
     except Exception as e:
         st.error(f"Error: {e}")
 else:
