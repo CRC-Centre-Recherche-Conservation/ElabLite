b72666020bf63f06f9a993e5b1ce2c52716b86dc
rayondemiel <maximehumeau44@gmail.com>
2024-05-22 12:21:22 +0200

    Add version menu sidebar

---
+++ __version__.py
@@ -0,0 +1,3 @@
+__version__ = "0.0.1"
+__published__ = "2024-05-06"
+__identifier__ = "%s (%s)" % (__version__, __published__)
--- app.py
+++ app.py
@@ -1,18 +1,33 @@
+import logging
+import os
 import streamlit as st
 
 from utils.menu import menu
 
-# APP
-st.set_page_config(page_title="ElabLite", )
-st.title("ElabLite")
-st.write(
-    "Welcome to ElabLite like metadata generator! This tool helps you generate metadata for use in your elabFTW instance with easy templates.")
+# Logs
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# init step metadata
-if "step_metadata" not in st.session_state:
-    st.session_state["step_metadata"] = "step_metadata_base"
-if 'validation_error' not in st.session_state:
-    st.session_state.validation_error = True
 
-#SIDEBAR
-menu()
+def run():
+    """
+    Run app - homepage
+    :return:
+    """
+    st.set_page_config(page_title="ElabLite", )
+    st.title(f"ElabLite")
+    st.write(
+        "Welcome to ElabLite like metadata generator! This tool helps you generate metadata for use in your elabFTW instance with easy templates.")
+
+    # init step metadata
+    if "step_metadata" not in st.session_state:
+        st.session_state["step_metadata"] = "step_metadata_base"
+    if 'validation_error' not in st.session_state:
+        st.session_state.validation_error = True
+
+    # SIDEBAR
+    menu()
+
+
+if __name__ == "__main__":
+    run()
