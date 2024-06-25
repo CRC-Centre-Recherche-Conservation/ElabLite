0d2cba92b3ea4dbf22b53ca004dba6d745028253
rayondemiel <maximehumeau44@gmail.com>
2024-06-25 13:35:04 +0200

    #18 - init dividing functionality metadata process

--- utils/menu.py
+++ utils/menu.py
@@ -10,10 +10,11 @@ def menu():
         st.sidebar.image('static/icons/logo.svg')
         st.sidebar.markdown(f"<p style='text-align:center'><i>Version: {VERSION}</i></p>", unsafe_allow_html=True)
     with st.sidebar.container():
-        st.sidebar.title("ElabLite")
         st.sidebar.page_link("app.py", label="Homepage")
         st.sidebar.title("Metadata")
         st.sidebar.page_link("pages/1-select_template.py", label="Select template")
         if "selected_template" in st.session_state and st.session_state["selected_template"] is not None:
             st.sidebar.page_link("pages/2-metadata_forms.py", label="Complete metadata")
+        st.sidebar.title("Metadata & data files")
+        st.sidebar.page_link("pages/3-metadata_management.py", label="Select preset")
         st.sidebar.divider()
