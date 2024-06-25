6e2941ce46c14214ec84823ab01a3f00b1c80df5
rayondemiel <maximehumeau44@gmail.com>
2024-06-25 13:47:38 +0200

    add icon homepage

--- utils/menu.py
+++ utils/menu.py
@@ -10,7 +10,7 @@ def menu():
         st.sidebar.image('static/icons/logo.svg')
         st.sidebar.markdown(f"<p style='text-align:center'><i>Version: {VERSION}</i></p>", unsafe_allow_html=True)
     with st.sidebar.container():
-        st.sidebar.page_link("app.py", label="Homepage")
+        st.sidebar.page_link("app.py", label="Homepage", icon="🏠")
         st.sidebar.title("Metadata")
         st.sidebar.page_link("pages/1-select_template.py", label="Select template")
         if "selected_template" in st.session_state and st.session_state["selected_template"] is not None:
