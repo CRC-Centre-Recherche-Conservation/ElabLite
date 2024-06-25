a456331c24efd2d773f0ea1503e36c6095235793
rayondemiel <maximehumeau44@gmail.com>
2024-06-25 14:44:26 +0200

    #18 - rename file + manage menu

--- utils/menu.py
+++ utils/menu.py
@@ -14,7 +14,8 @@ def menu():
         st.sidebar.title("Metadata")
         st.sidebar.page_link("pages/1-select_template.py", label="Select template")
         if "selected_template" in st.session_state and st.session_state["selected_template"] is not None:
-            st.sidebar.page_link("pages/2-metadata_forms.py", label="Complete metadata")
+            st.sidebar.page_link("pages/2-metadata_forms.py", label="Make preset")
         st.sidebar.title("Metadata & data files")
-        st.sidebar.page_link("pages/3-metadata_management.py", label="Select preset")
+        st.sidebar.page_link("pages/3-metadata_preset.py", label="Select preset/templates")
+        st.sidebar.page_link("pages/4-metadata_management.py", label="Complete experience(s)")
         st.sidebar.divider()
