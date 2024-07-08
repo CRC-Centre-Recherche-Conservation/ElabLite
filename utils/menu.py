88c43d197ef4e8cd055fc6fd177d293d731b7f66
rayondemiel <maximehumeau44@gmail.com>
2024-07-08 14:28:19 +0200

    Renaming menu

--- utils/menu.py
+++ utils/menu.py
@@ -11,12 +11,13 @@ def menu():
         st.sidebar.markdown(f"<p style='text-align:center'><i>Version: {VERSION}</i></p>", unsafe_allow_html=True)
     with st.sidebar.container():
         st.sidebar.page_link("app.py", label="Homepage", icon="🏠")
-        st.sidebar.title("Metadata")
-        st.sidebar.page_link("pages/1-select_template.py", label="Select template")
+        st.sidebar.title("Metadata Experiment")
+        st.sidebar.page_link("pages/1-select_template.py", label="New Experiment")
+        st.sidebar.page_link("pages/5-load_template.py", label="Load Experiment")
         if "selected_template" in st.session_state and st.session_state["selected_template"] is not None:
-            st.sidebar.page_link("pages/2-metadata_forms.py", label="Make preset")
-        st.sidebar.title("Metadata & data files")
-        st.sidebar.page_link("pages/3-metadata_preset.py", label="Select preset/template")
+            st.sidebar.page_link("pages/2-metadata_forms.py", label="Current Experiment")
+        st.sidebar.title("Files Experiment")
+        st.sidebar.page_link("pages/3-metadata_preset.py", label="Select Experiment")
         if "selected_preset" in st.session_state and st.session_state["selected_preset"] is not None:
-            st.sidebar.page_link("pages/4-metadata_management.py", label="Complete experience(s)")
+            st.sidebar.page_link("pages/4-metadata_management.py", label="Complete Experiment(s)")
         st.sidebar.divider()
