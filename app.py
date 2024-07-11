aeb4755693c3b5f9c5d2f3caa86f9ed7bbe8332d
rayondemiel <maximehumeau44@gmail.com>
2024-07-11 12:23:59 +0200

    #33 - expander menu

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def run():
     Run app - homepage
     :return:
     """
-    st.set_page_config(page_title="ElabLite", )
+    st.set_page_config(page_title="ElabLite")
     st.title(f"ElabLite")
     st.write(
         "Welcome to ElabLite like metadata generator! This tool helps you generate metadata for use in your elabFTW instance with easy templates.")
--- utils/menu.py
+++ utils/menu.py
@@ -2,22 +2,43 @@ import streamlit as st
 
 from __version__ import __identifier__ as VERSION
 
+
 def menu():
     """
     Menu sidebar
     """
+    st.markdown(
+        '''
+        <style>
+        [data-testid="stSidebar"] [data-testid="stExpander"] details {
+        border: none !important;
+        }
+        div[data-testid="stExpander"] details summary p {
+        font-size: 2rem;
+        font-weight: bold;
+        }
+        div[data-testid="stExpander"] details summary svg {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        }
+        </style>
+        ''',
+        unsafe_allow_html=True
+    )
     with st.sidebar.container():
         st.sidebar.image('static/icons/logo.svg')
         st.sidebar.markdown(f"<p style='text-align:center'><i>Version: {VERSION}</i></p>", unsafe_allow_html=True)
     with st.sidebar.container():
         st.sidebar.page_link("app.py", label="Homepage", icon="🏠")
-        st.sidebar.title("Metadata Experiment")
-        st.sidebar.page_link("pages/1-select_template.py", label="New Experiment")
-        st.sidebar.page_link("pages/1-load_template.py", label="Load Experiment")
+    with st.sidebar.expander("Experiment Menu", expanded=True):
+        st.subheader("Metadata Experiment")
+        st.page_link("pages/1-select_template.py", label="• New Experiment")
+        st.page_link("pages/1-load_template.py", label="• Load Experiment")
         if "selected_template" in st.session_state and st.session_state["selected_template"] is not None:
-            st.sidebar.page_link("pages/2-metadata_forms.py", label="Current Experiment")
-        st.sidebar.title("Files Experiment")
-        st.sidebar.page_link("pages/3-metadata_preset.py", label="Select Experiment")
+            st.page_link("pages/2-metadata_forms.py", label="• Current Experiment")
+        st.subheader("Files Experiment")
+        st.page_link("pages/3-metadata_preset.py", label="• Select Experiment")
         if "selected_preset" in st.session_state and st.session_state["selected_preset"] is not None:
-            st.sidebar.page_link("pages/4-metadata_management.py", label="Complete Experiment(s)")
-        st.sidebar.divider()
+            st.page_link("pages/4-metadata_management.py", label="• Complete Experiment(s)")
+    st.sidebar.divider()
